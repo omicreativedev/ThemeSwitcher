f04dbe53b943a56db1bc9f29b69b52b340492a87
OmiCreativeDev <s.shunamon@gmail.com>
2023-10-05 22:58:58 -0400

    PHP to JS

--- nav.js
+++
@@ -1,7 +0,0 @@
-function changePage() {
-  var selectBox = document.getElementById("page-select");
-  var selectedValue = selectBox.options[selectBox.selectedIndex].value;
-  if (selectedValue !== "") {
-    window.location.href = selectedValue;
-  }
-}
